07199ef9e7d64867b774e75db54ae6478a3a5981
Yami <yamilethmedina1@gmail.com>
2016-01-31 23:21:05 -0500

    changed redirect uri for heroku deployment

--- public/finalproject/app.js
+++ public/finalproject/app.js
@@ -1,3 +1,3 @@
 var my_client_id = 'd137fe25b31c4f3ba9e29d85f4e47c66'; // Your client id
 var my_secret = '044d1250a8e74f8481b20cf3ad3316ee'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://whatshouldilistentotoday.herokuapp.com/finalproject/callback/'; // Your redirect uri
